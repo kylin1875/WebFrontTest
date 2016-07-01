6ba3566ef1a746183eab36aee06606b4e1920fc6
kylin1875 <zhiweizheng@Zhiweis-MacBook-Pro.local>
2016-06-30 17:24:26 -0700

    mid

---
+++ mid.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>Midterm Exam
+	</head>
+	<script>
+
+	</script>
+	<body>
+		<table border="1"  height="100">
+			
+		<tr>
+		<th width="300" colspan=3 rowspan=1 id="0">x</th>
+		</tr>
+		
+		<tr>
+		<th width="100" colspan=1 rowspan=1>y</th>
+		<th width="200" colspan=2 rowspan=1>z</th>
+		</tr>		
+		
+		<tr>
+		<th width="300" colspan=3 rowspan=1>m</th>
+		</tr>
+		
+		</table>
+	</body>
+</html>
